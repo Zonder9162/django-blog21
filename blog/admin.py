32aa6d743ee802afcd024083a075d4650eda37c0
Zonder9162 <zonder9162@gmail.com>
2022-07-26 12:55:23 +0600

    models

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from atexit import register
+from django.contrib import admin
+
+from .models import Post, Comment, Like
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
